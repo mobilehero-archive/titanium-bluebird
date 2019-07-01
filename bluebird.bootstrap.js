6281b65553a6201c74467307923f3eb6106d6de1
Brenton House <brenton.house@gmail.com>
2019-07-01 15:14:38 -0500

    :checkered_flag: v3.0.0-4 :heavy_minus_sign: Repackaging from: https://www.npmjs.com/package/bluebird

--- bluebird.bootstrap.js
+++ bluebird.bootstrap.js
@@ -1,2 +1,4 @@
 require('/ti.internal/extensions/binding')('bluebird', '/node_modules/@titanium/bluebird');
+// WARNING:  This code below does not work when working with LiveView.  
+// When using LiveView,  you must include the line below in your alloy.js file
 global.Promise = require('@titanium/bluebird');
