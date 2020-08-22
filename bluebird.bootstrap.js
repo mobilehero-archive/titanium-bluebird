6299dbc7ff9ddd7e566a8726ad3e97cbf7a18427
Brenton House <brenton.house@gmail.com>
2020-08-22 17:46:44 -0500

    Add fix for process.toString() bug

--- bluebird.bootstrap.js
+++ bluebird.bootstrap.js
@@ -1,6 +1,4 @@
-// code for SDK < 8.1.0.GA
-// require('/ti.internal/extensions/binding')('bluebird', '/node_modules/@titanium/bluebird');
-
-// Code for SDK >= 8.1.0.GA
+// TIBUG:  Fix for https://jira.appcelerator.org/browse/TIMOB-28094
+global.process[Symbol.toStringTag] = 'process';
 global.binding.register('bluebird', require('@titanium/bluebird'));
 
